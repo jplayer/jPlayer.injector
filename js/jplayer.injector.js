e7764e0d619b1d9b12bf183caf2b8ee8020d8513
Happyworm <markp@happyworm.com>
2013-09-27 00:29:22 +0100

    Added jshint options and debugged to get clean pass.

--- js/jplayer.injector.js
+++ js/jplayer.injector.js
@@ -1,4 +1,25 @@
+/*
+ * jPlayer Injector
+ * http://www.jplayer.org
+ *
+ * Copyright (c) 2013 Happyworm Ltd
+ * Licensed under the MIT license.
+ * http://opensource.org/licenses/MIT
+ *
+ * Author: Mark J Panaghiston
+ * Version: 0.0.1
+ * Date: 26th September 2013
+ */
+
+/* Code verified using http://www.jshint.com/ */
+/*jshint asi:false, bitwise:false, boss:false, debug:false, eqeqeq:true, eqnull:false, evil:false, forin:false, immed:false, laxbreak:false, newcap:true, noarg:true, noempty:true, nonew:true, plusplus:false, regexp:false, undef:true, sub:false, strict:true, smarttabs:true */
+/*jshint browser:true, devel:true, jquery:true */
+/*jshint curly:false  */
+/*exported jPlayerInjector */
+
 var jPlayerInjector = (function($) {
+
+	"use strict";
 
 	var DEBUG = false;
 
@@ -36,9 +57,9 @@ var jPlayerInjector = (function($) {
 			if(DEBUG) console.log('loaded: %s', options.template);
 
 			// Iterate through each selector instance
-			$(options.selector).each(function(key, val) {
+			$(options.selector).each(function(index) {
 
-				if(DEBUG) console.log('injecting: %s #%d', options.selector, key);
+				if(DEBUG) console.log('injecting: %s #%d', options.selector, index);
 
 				var $this = $(this),
 					media = {
@@ -61,24 +82,24 @@ var jPlayerInjector = (function($) {
 					},
 					supplied =
 						// Audio codecs
-						(media.mp3 ? "mp3," : "")
-						+ (media.m4a ? "m4a," : "")
-						+ (media.oga ? "oga," : "")
-						+ (media.wav ? "wav," : "")
-						+ (media.webma ? "webma," : "")
-						+ (media.fla ? "fla," : "")
-						+ (media.rtmpa ? "rtmpa," : "")
+						(media.mp3 ? "mp3," : "") +
+						(media.m4a ? "m4a," : "") +
+						(media.oga ? "oga," : "") +
+						(media.wav ? "wav," : "") +
+						(media.webma ? "webma," : "") +
+						(media.fla ? "fla," : "") +
+						(media.rtmpa ? "rtmpa," : "") +
 						// Video codecs
-						+ (media.m4v ? "m4v," : "")
-						+ (media.ogv ? "ogv," : "")
-						+ (media.webmv ? "webmv," : "")
-						+ (media.flv ? "flv," : "")
-						+ (media.rtmpv ? "rtmpv," : ""),
+						(media.m4v ? "m4v," : "") +
+						(media.ogv ? "ogv," : "") +
+						(media.webmv ? "webmv," : "") +
+						(media.flv ? "flv," : "") +
+						(media.rtmpv ? "rtmpv," : ""),
 					jPlayerOptions = $.extend({}, options.jPlayer, {
 						ready: function () {
 							$(this).jPlayer("setMedia", media);
 						},
-						cssSelectorAncestor: "#" + options.prefix.cssSelectorAncestor + key,
+						cssSelectorAncestor: "#" + options.prefix.cssSelectorAncestor + index,
 						supplied: supplied
 					}),
 					impression = "";
@@ -86,21 +107,21 @@ var jPlayerInjector = (function($) {
 				if(options.pauseOthers) {
 					jPlayerOptions.play = function() {
 						$(this).jPlayer("pauseOthers");
-					}
+					};
 				}
 
 				// Switch the markers with the values for this instance.
-				impression = template.replace(options.marker.jPlayer, options.prefix.jPlayer + key);
-				impression = impression.replace(options.marker.cssSelectorAncestor, options.prefix.cssSelectorAncestor + key);
+				impression = template.replace(options.marker.jPlayer, options.prefix.jPlayer + index);
+				impression = impression.replace(options.marker.cssSelectorAncestor, options.prefix.cssSelectorAncestor + index);
 				impression = impression.replace(options.marker.title, $this.data("title"));
 				$this.html(impression);
 
-				if(DEBUG) console.log('media#%d: %o', key, media);
-				if(DEBUG) console.log('supplied#%d: %o', key, supplied);
-				if(DEBUG) console.log('jPlayerOptions#%d: %o', key, jPlayerOptions);
+				if(DEBUG) console.log('media#%d: %o', index, media);
+				if(DEBUG) console.log('supplied#%d: %o', index, supplied);
+				if(DEBUG) console.log('jPlayerOptions#%d: %o', index, jPlayerOptions);
 
 				// Instance jPlayer
-				$("#" + options.prefix.jPlayer + key).each(function() {
+				$("#" + options.prefix.jPlayer + index).each(function() {
 					$(this).jPlayer(jPlayerOptions);
 				});
 			});
