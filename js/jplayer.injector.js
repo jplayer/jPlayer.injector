1bed8644658eca3ee463b3d435d0d9e1b32ada2a
Mark Boas <MarkB@Happyworm.com>
2014-04-01 17:10:59 +0200

    changes made to cope with multipe source definitions

--- js/jplayer.injector.js
+++ js/jplayer.injector.js
@@ -21,7 +21,7 @@ var jPlayerInjector = (function($) {
 
 	"use strict";
 
-	var DEBUG = true;
+	var DEBUG = false;
 
 	return function(options) {
 
@@ -49,36 +49,56 @@ var jPlayerInjector = (function($) {
 			// Iterate through each selector instance
 			$(options.selector).each(function(index) {
 
+				var $this = $(this);
+
 				if(DEBUG) console.log('injecting: %s #%d', options.selector, index);
 
-				if(DEBUG) console.log("src = "+ $(this).attr('src'));
+				var mediaList = [];
 
-				var $this = $(this),
-					tagname = $this.prop('tagName'),
-					type = $this.attr('type') ? $this.attr('type').replace(" ","") : "",
-					filename = $this.attr('src'),
-					extension = filename.split('.').pop(),
-					media = { // Grouped by type (audio/video), but ordered with essential last.
+				// Todo : remove repeated code here
+				var media = {src:$(this).attr("src"),type:$(this).attr("type")};
+				if (media) mediaList.push(media);
+
+				$(this).children("source").map(function(){
+					media={src:$(this).attr("src"),type:$(this).attr("type")};
+					if (media) mediaList.push(media);
+				});
+
+				var assignMedia = function(tagType, mimeType, variants) {
+					var assign = "";
+					if ($this.prop('tagName').toLowerCase() == tagType) {
+
+						for (var i=0; i < mediaList.length; i++) {
+							var filename = mediaList[i].src;
+							var extension = filename ? filename.split('.').pop() : "";
+
+							if ((mediaList[i].type && mediaList[i].type.toLowerCase().replace(" ","") == mimeType) || (variants.indexOf(extension) >= 0)) {
+								assign = filename;
+							}
+						}
+					}
+					return assign;
+				};
+
+				var media = { // Grouped by type (audio/video), but ordered with essential last.
 						// Audio codecs
-						rtmpa: (tagname =="AUDIO" && (extension == "rtmpa" || extension == "rtmp") ? filename: ""),
-						fla: (tagname =="AUDIO" && extension == "fla" ? filename: ""),
-						wav: (tagname =="AUDIO" && (type == "audio/wav" || extension == "wav") ? filename: ""),
-						webma: (tagname =="AUDIO" && (type == "audio/webm" || extension == "webma" || extension == "webm") ? filename: ""),
-						oga: (tagname =="AUDIO" && (type == "audio/ogg" || extension == "oga" || extension == "ogg") ? filename: ""),
-						m4a: (tagname =="AUDIO" && (type == "audio/mp4" || extension == "m4a" || extension == "mp4") ? filename: ""),
-						mp3: (tagname =="AUDIO" && (type == "audio/mpeg" || extension == "mp3") ? filename: ""),
+						rtmpa: assignMedia("audio","",["rtmpa,rtmp"]),
+						fla:   assignMedia("audio","",["fla"]),
+						wav:   assignMedia("audio","audio/wav",["wav"]),
+						webma: assignMedia("audio","audio/webm",["webma","webm"]),
+						oga:   assignMedia("audio","audio/ogg",["oga","ogg"]),
+						m4a:   assignMedia("audio","audio/mp4",["m4a","mp4"]),
+						mp3:   assignMedia("audio","audio/mpeg",["mp3"]),
 
 						// Video codecs
-						rtmpa: (tagname =="VIDEO" && (extension == "rtmpv" || extension == "rtmp") ? filename: ""),
-						flv: (tagname =="VIDEO" && extension == "flv" ? filename: ""),
-						webmv: (tagname =="VIDEO" && (type == "video/webm" || extension == "webmv" || extension == "webm") ? filename: ""),
-						ogv: (tagname =="VIDEO" && (type == "video/ogg" || extension == "ogv" || extension == "ogg") ? filename: ""),
-						m4v: (tagname =="VIDEO" && (type == "video/mp4" || extension == "m4v" || extension == "mp4") ? filename: ""), // Essential
+						rtmpv: assignMedia("video","",["rtmpv,rtmp"]),
+						flv:   assignMedia("video","",["flv"]),
+						webmv: assignMedia("video","video/webm",["webmv","webm"]),
+						ogv:   assignMedia("video","video/ogg",["oqv","ogg"]),
+						m4v:   assignMedia("video","video/mp4",["m4v","mp4"]),
 
 						// Poster
 						poster: $this.data("poster")
-
-
 					},
 					supplied =
 						// Audio codecs
@@ -114,12 +134,9 @@ var jPlayerInjector = (function($) {
 				impression = template.replace(options.marker.jPlayer, options.prefix.jPlayer + index);
 				impression = impression.replace(options.marker.cssSelectorAncestor, options.prefix.cssSelectorAncestor + index);
 				impression = impression.replace(options.marker.title, $this.data("title"));
-				//$this.html(impression);
+
 				$this.replaceWith(impression);
 
-				if(DEBUG) console.log('tagname', tagname);
-
-				if(DEBUG) console.log('extension', options.template);
 				if(DEBUG) console.log('media#%d: %o', index, media);
 				if(DEBUG) console.log('supplied#%d: %s', index, jPlayerOptions.supplied);
 				if(DEBUG) console.log('jPlayerOptions#%d: %o', index, jPlayerOptions);
